e1b0aa63bbc766ebae71857fc4330ecec2eb556f
Anton714 <an714.am@gmail.com>
2024-09-22 16:38:33 +0500

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,14 +1,10 @@
 def add_everything_up(a, b):
     try:
-        y = (a + b)
-
+        return a + b
 
     except TypeError:
-        y = (str(a) + str(b))
-
-    return y
-
-
+        return str(a) + str(b)
+    
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
